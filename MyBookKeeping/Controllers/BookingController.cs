77f98ea7e8070429a88a0ec7a97755690b1fd1da
ewingtsui <ewing.tsui@gmail.com>
2017-03-08 22:05:45 +0800

    2-test
    Signed-off-by: ewingtsui <ewing.tsui@gmail.com>

--- MyBookKeeping/Controllers/BookingController.cs
+++ MyBookKeeping/Controllers/BookingController.cs
@@ -3,33 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using MyBookKeeping.Models;
-using MyBookKeeping.Models.ViewModels;
+
+using MyBookKeeping.Models.Service;
 
 namespace MyBookKeeping.Controllers
 {
     public class BookingController : Controller
     {
-        static ProcessViewModel DataModel = new ProcessViewModel();
-        List<BookingsModels> ListDetail = DataModel.InitData();
+        AccountBookService _dbService = new AccountBookService();
 
         // GET: Booking
         public ActionResult Index()
         {
-            return View(ListDetail);
+            var source = _dbService.AccountBookList();
+            return View(source);
         }
 
         // GET: Booking/Create
         public ActionResult Create()
         {
-            return View(ListDetail);
+            return View();
         }
 
         // POST: Booking/Create
         [HttpPost]
-        public ActionResult Create(BookingsModels collection)
+        public ActionResult Create(FormCollection collection)
         {
-            return View(ListDetail);
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
---
+++ MyBookKeeping/Models/Service/AccountBookService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using MyBookKeeping.Models;
+using MyBookKeeping.Models.ViewModels;
+
+namespace MyBookKeeping.Models.Service
+{
+    public class AccountBookService
+    {
+        private SkillTreeHomeworkEntities db = new SkillTreeHomeworkEntities();
+        
+        public List<BookingsViewModels> AccountBookList()
+        {
+            List<BookingsViewModels> LastDetail = new List<BookingsViewModels>();
+
+            foreach(var item in db.AccountBook.OrderBy(d=>d.Id).ToList())
+            {
+                BookingsViewModels GetAccountBook = new BookingsViewModels();
+
+                if (item.Categoryyy == 0)
+                {
+                    GetAccountBook.BookType = "支出";
+                }
+                else
+                {
+                    GetAccountBook.BookType = "收入";
+                }
+
+                GetAccountBook.BookAmount = item.Amounttt;
+                GetAccountBook.BookDate = item.Dateee;
+                GetAccountBook.BookMemo = item.Remarkkk;
+
+                LastDetail.Add(GetAccountBook);
+            }
+
+            return LastDetail;
+        }
+    }
+}
