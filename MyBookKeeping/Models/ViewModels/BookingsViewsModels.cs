211ad842b47e8c4c99d6e8e74c500460bfd955d9
ewingtsui <ewing.tsui@gmail.com>
2017-03-08 22:05:45 +0800

    2-test
    Signed-off-by: ewingtsui <ewing.tsui@gmail.com>

---
+++ MyBookKeeping/Models/ViewModels/BookingsViewsModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyBookKeeping.Models.ViewModels
+{
+    public class BookingsViewModels
+    {
+        public Guid ID { get; set; }
+        public string BookType { get; set; }
+        public DateTime BookDate { get; set; }
+        public Decimal BookAmount { get; set; }
+        public string BookMemo { get; set; }
+    }
+}
