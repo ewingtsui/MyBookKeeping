b47365f600fea4cf9f7c8879a55a12753e3903b8
ewingtsui <ewing.tsui@gmail.com>
2017-03-21 21:15:50 +0800

    6.將Create改成 Html Helper

--- MyBookKeeping/Models/ViewModels/BookingsViewsModels.cs
+++ MyBookKeeping/Models/ViewModels/BookingsViewsModels.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyBookKeeping.Models.ViewModels
 {
     public class BookingsViewModels
     {
         public Guid ID { get; set; }
+
+        [Display(Name = "類別")]
         public string BookType { get; set; }
+        
+        [Display(Name = "日期")]
+        [Required]
+        [Remote("Valid", "Accounting", ErrorMessage = "日期不可大於今天")]
         public DateTime BookDate { get; set; }
+        
+        [Display(Name = "金額")]
+        [Required]
+        [Range(1, int.MaxValue)]
         public Decimal BookAmount { get; set; }
+        
+        [Display(Name = "備註")]
         public string BookMemo { get; set; }
     }
 }
