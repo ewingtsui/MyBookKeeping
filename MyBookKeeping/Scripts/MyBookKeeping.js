fe4ae074340d1a47a97ca26dbccc96d70b49dcaa
ewingtsui <ewing.tsui@gmail.com>
2017-03-21 21:40:35 +0800

    6.將Create的部份改成 Html Helper, 但日期的datepicker還沒做好

---
+++ MyBookKeeping/Scripts/MyBookKeeping.js
@@ -0,0 +1,10 @@
+﻿$("#_date").datepicker({
+    changeMonth: true,
+    changeYear: true,
+    dateFormat: "yy/mm/dd",
+    dayNamesMin: ["日", "一", "二", "三", "四", "五", "六"],
+    monthNamesShort: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
+    showWeek: true,
+    firstDay: 0,
+    weekHeader: "周"
+});
